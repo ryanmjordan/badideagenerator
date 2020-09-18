e205b1dd396df346d2886b33a6f0d836490acece
ryanmjordan <jordarn96@gmail.com>
2020-09-18 18:40:30 -0400

    initial python code

---
+++ pythonproject/main.py
@@ -0,0 +1,8 @@
+print("Would you like an idea? (Y/N)")
+ans = input()
+if ans == "Y" or ans == "y":
+    print("You and me both")
+elif ans == "N" or ans == "n":
+    print("Thanks for wasting my time")
+else:
+    print("I don't think I can help you")
