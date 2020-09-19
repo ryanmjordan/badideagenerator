fcc299419cb11684c523be0432854f0e4f8adc5b
ryanmjordan <jordarn96@gmail.com>
2020-09-19 03:15:03 -0400

    have created basic Flask web app

--- pythonproject/application.py
+++ pythonproject/application.py
@@ -1,7 +1,34 @@
+from flask import Flask, redirect, url_for, render_template
 import time
 import sys
 from tqdm import tqdm
 import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Random first verb
+    firstVerbs = ["Hack ", "Innovate ", "Monetize ", "Create "]
+    firstVerbRandom = random.randint(0, 3)
+
+    # Random Noun
+    f = open("nounlist.txt")
+    nounLines = f.readlines()
+    lineCount = len(open("nounlist.txt").readlines())
+    randomNoun1 = random.randint(0, lineCount)
+    randomNoun2 = random.randint(0, lineCount)
+    randomNoun3 = random.randint(0, lineCount)
+
+    fullSentence = firstVerbs[firstVerbRandom] + nounLines[randomNoun1] + \
+                   " with " + nounLines[randomNoun2] + " and " \
+                   + nounLines[randomNoun3]
+
+    return render_template("index.html", fullSentence=fullSentence)
+
+
+if __name__ == "__main__":
+    app.run()
 
 print("Would you like an idea? (Y/N)")
 ans = input()
