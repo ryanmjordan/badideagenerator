f4ebf439a7f12c11982498b82de04b3d61f375cd
ryanmjordan <jordarn96@gmail.com>
2020-09-19 01:28:33 -0400

    basic text based version of program

--- pythonproject/main.py
+++ pythonproject/main.py
@@ -1,7 +1,32 @@
+import time
+import sys
+from tqdm import tqdm
+import random
+
 print("Would you like an idea? (Y/N)")
 ans = input()
 if ans == "Y" or ans == "y":
-    print("You and me both")
+    print("Generating...")
+    # Random first verb
+    firstVerbs = ["Hack ", "Innovate ", "Monetize ", "Create "]
+    firstVerbRandom = random.randint(0,3)
+
+    # Random Noun
+    f = open("nounlist.txt")
+    nounLines = f.readlines()
+    lineCount = len(open("nounlist.txt").readlines(  ))
+    randomNoun1 = random.randint(0,lineCount)
+    randomNoun2 = random.randint(0, lineCount)
+    randomNoun3 = random.randint(0, lineCount)
+
+    pbar = tqdm(total=100)
+    for i in range(10):
+        time.sleep(0.1)
+        pbar.update(10)
+    pbar.close()
+    print("")
+    print("Here is your great idea:")
+    print(firstVerbs[firstVerbRandom] + nounLines[randomNoun1] + " with " + nounLines[randomNoun2] + " and " + nounLines[randomNoun3])
 elif ans == "N" or ans == "n":
     print("Thanks for wasting my time")
 else:
